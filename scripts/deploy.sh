b72a09ec227b6fcfe757608655a975057c8a599b
nandananr <nandana.nalapareddy@gmail.com>
2017-08-14 11:54:23 -0500

    replace https:// schema in deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -16,7 +16,8 @@ export PATH=$PATH:$HOME/.local/bin # put aws in the path
 envsubst < task-definition.json > new-task-definition.json
 
 aws_login=$(aws ecr get-login --region $AWS_DEFAULT_REGION) #needs AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY envvars
-eval $(aws_login.replace('https://',''))
+print(aws_login)
+eval $(aws_login.replace("https://",""))
 
 #docker login -u AWS -p <password> -e none <aws_account_id>.dkr.ecr.<region>.amazonaws.com
 #docker login -u AWS -p AWS_SECRET_KEY -e none AWS_ECS_REPO_DOMAIN
