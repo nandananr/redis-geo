d56533a9796accef51fd43d54e624852c08e44e9
nandananr <nandana.nalapareddy@gmail.com>
2017-08-15 08:04:59 -0500

    change 24

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -14,6 +14,8 @@ export PATH=$PATH:$HOME/.local/bin # put aws in the path
 # replace environment variables in task-definition
 envsubst < task-definition.json > new-task-definition.json
 
+eval $(aws ecr get-login --region $AWS_DEFAULT_REGION) #needs AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY envvars
+
 #aws_login=$(aws ecr get-login --region $AWS_DEFAULT_REGION) #needs AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY envvars
 #replacedLogin = $aws_login.replace("https://", "")
 #aws_loginstr = (str($aws_login))
@@ -23,7 +25,7 @@ envsubst < task-definition.json > new-task-definition.json
 
 #eval $(("'%s'" % $aws_login).replace('https://',''))
 
-docker login -u AWS -p 3O2HsQ3Tc1nj7+rfMBjU1R+zmyUryxpBMV8AzGTv -e none $AWS_ECS_REPO_DOMAIN
+#docker login -u AWS -p 3O2HsQ3Tc1nj7+rfMBjU1R+zmyUryxpBMV8AzGTv -e none $AWS_ECS_REPO_DOMAIN
 #docker login -u AWS -p AWS_SECRET_KEY -e none AWS_ECS_REPO_DOMAIN
 
 if [ $(aws ecr describe-repositories | jq --arg x $IMAGE_NAME '[.repositories[] | .repositoryName == $x] | any') == "true" ]; then
