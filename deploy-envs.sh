5a08094cd73029f3f74018f809a2641d39fbd136
nandananr <nandana.nalapareddy@gmail.com>
2017-08-15 11:06:40 -0500

    change 26

--- deploy-envs.sh
+++ deploy-envs.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # set environment variables used in deploy.sh and AWS task-definition.json:
-export IMAGE_NAME=ecsdemo-redisgeo
+export IMAGE_NAME=ecsdemo-redisgeo-sample
 export IMAGE_VERSION=latest
 
-export AWS_DEFAULT_REGION=us-east-1
+export AWS_DEFAULT_REGION=us-east-2
 export AWS_ECS_CLUSTER_NAME=default
-export AWS_VIRTUAL_HOST=34.231.5.225
+export AWS_VIRTUAL_HOST=18.220.138.117
 
 # set any sensitive information in travis-ci encrypted project settings:
 # required: AWS_ACCOUNT_NUMBER, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY
