588a42bdbd4a30fc2f268b6e1216c153cb308982
nandananr <nandana.nalapareddy@gmail.com>
2017-08-29 07:36:57 -0500

    change 35

--- Dockerfile
+++ Dockerfile
@@ -5,5 +5,5 @@ RUN ["dotnet", "restore"]
 WORKDIR /app/RedisGeo
 RUN ["dotnet", "build"]
 EXPOSE 5000/tcp
-ENV ASPNETCORE_URLS https://*:5000
+ENV ASPNETCORE_URLS http://*:5000
 ENTRYPOINT ["dotnet", "run", "--server.urls", "http://*:5000"]
--- src/RedisGeo/Program.cs
+++ src/RedisGeo/Program.cs
@@ -14,7 +14,7 @@ namespace RedisGeo
             var host = new WebHostBuilder()
                 .UseKestrel()
                 .UseContentRoot(Directory.GetCurrentDirectory())
-               // .UseIISIntegration()
+                .UseIISIntegration()
                 .UseStartup<Startup>()
                 .Build();
 
