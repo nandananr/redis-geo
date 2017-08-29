f9cac83bcd65281ab78106926393338e8f29af28
nandananr <nandana.nalapareddy@gmail.com>
2017-08-29 08:13:33 -0500

    change 36

--- src/RedisGeo/Program.cs
+++ src/RedisGeo/Program.cs
@@ -4,21 +4,20 @@ using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore;
 
 namespace RedisGeo
 {
     public class Program
     {
-        public static void Main(string[] args)
+         public static void Main(string[] args)
         {
-            var host = new WebHostBuilder()
-                .UseKestrel()
-                .UseContentRoot(Directory.GetCurrentDirectory())
-                .UseIISIntegration()
+            BuildWebHost(args).Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
                 .Build();
-
-            host.Run();
-        }
     }
 }
